3e87d2e745bcb5ed6132e038455da62f8d505284
alejomarco <alejom1999@gmail.com>
2021-08-23 21:13:10 -0300

    cambie todos los archivos que tenia para hacer el desafio anterior y los reemplace por el proyecto final

---
+++ index.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector (".toggle")
+const navMenu = document.querySelector (".nav-menu")
+
+toggle.addEventListener("click", ()=>{
+    navMenu.classList.toggle("nav-menu_visible")
+}
+)
